1ace0f8cbb71eded07682477c527c2d786a9f534
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-03-04 13:39:05 -0600

    Added comments. Calculator can now use the result of the previous equation in the next equation. Changed the deleteDigit function to remove empty spaces and reset operator value when necessary, delete now functions as intended.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -14,35 +14,58 @@ function onReady() {
     getAllEquations();
 }
 
+// Global variables
 let firstOperand = '';
 let secondOperand = '';
 let operator = '';
-
 let currentEquation = '';
 let lastEquation = '';
 
+// Allows user to run past equations in history with var\
 function runPastEquation() {
     currentEquation = $(this).text();
     runCalculation();
 }
 
+// Allows user to delete most recent digit in equation
+    // If statements allows the user to delete operator and empty spaces
 function deleteDigit() {
+    // Store thing to delete
+    let thingToDelete = currentEquation[currentEquation.length - 1];
+    
+    // If it's an operator, remove it and the empty space before it. 
+        // Clear value stored in operator var
+    if (thingToDelete === '+' || thingToDelete === '-' || thingToDelete === 'x' || thingToDelete === '÷') {
+        currentEquation = currentEquation.slice(0, -2);
+        operator = ''
+    };
+
+     // If it's an empty space, remove it and the character before it. 
+        // If the previous character is an operator, clear the value stored in operator var.
+    if (thingToDelete === ' ') {
+        currentEquation = currentEquation.slice(0, -2);
+        operator = ''
+    }
+
+    // Else remove the first chara
     currentEquation = currentEquation.slice(0, -1);
     // console.log(currentEquation);
     $('#currentOperationScreen').text(currentEquation);
 }
 
-
 function runCalculation() {
+    // Create array out of currentEquation string
     let equationArr = currentEquation.split(' ');
     // console.log(equationArr);
     firstOperand = equationArr[0];
     operator = equationArr[1];
     secondOperand = equationArr[2];
 
+    // Only run POST request if its a valid equation
     if(firstOperand && secondOperand && operator) {
         lastEquation = currentEquation;
         currentEquation = '';
+        // Display last equation
         $('#lastOperationScreen').text(lastEquation);
     
         let equationObj = {
@@ -66,19 +89,10 @@ function clearAll() {
     $('#currentOperationScreen').text('');
 }
 
+// Updates currentEquation var with each number btn click and updates currentOperationScreen
 function getBtnNumber() {
     //console.log($(this).text());
     let num = $(this).text();
-    // if (operator.length === 0) {
-    //     firstOperand += num; 
-    //     currentEquation += num;
-    //     $('#currentOperationScreen').text(currentEquation);
-    // }
-    // else {
-    //     secondOperand+= num;
-    //     currentEquation += num;
-    //     $('#currentOperationScreen').text(currentEquation);
-    // }
 
     // Refactored to use current equation only
     currentEquation += num;
@@ -91,6 +105,8 @@ function getBtnNumber() {
     // console.log('current equation:', currentEquation);
 }
 
+// Updates operator var when operator btn clicked
+    // Only allows for single operation equations
 function getBtnOperator() {
     if(operator.length === 0) {
         operator = $(this).text();
@@ -114,6 +130,7 @@ function addNewEquation(equation){
     })
 }
 
+// Get the result of most recent equation
 function getEquationResult() {
     $.ajax({
         method: 'GET',
@@ -152,9 +169,11 @@ function deleteAllEquations() {
     })
 }
 
+// Updates currentEquation screen and allows equation result to be used for next equation
 function renderResult(equation) {
     console.log('Most recent equation solution', equation.solution);
     $('#currentOperationScreen').text(`${equation.solution}`);
+    currentEquation += equation.solution;
 }
 
 function renderAllEquations(equations) {
